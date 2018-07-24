93243b1d66c6aa808bbdf5de466b50783137ffa5
李楠楠 <nannan.li@tpadsz.cn>
2018-07-24 16:10:32 +0800

    修改新推荐位接口

--- src/main/java/com/web/controller/HPFeaturedController.java
+++ src/main/java/com/web/controller/HPFeaturedController.java
@@ -4,11 +4,8 @@ import com.model.HPFeatured;
 import com.model.dd.ResultDict;
 import com.service.HPFeaturedService;
 import org.apache.commons.collections.map.HashedMap;
-import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -18,15 +15,14 @@ import java.util.Map;
  * 新推荐位接口
  * Created by nannan.li on 2018/6/7.
  */
-@Controller
-@RequestMapping(value = "/home")
-public class HPFeaturedController extends BaseDecodedController {
+@Controller("hPFeaturedController")
+@RequestMapping("/home")
+public class HPFeaturedController {
 
     @Resource
     private HPFeaturedService hpFeaturedService;
 
-    @ResponseBody
-    @RequestMapping(value = "/getHPFeatured", produces = "application/json; charset=utf-8")
+    @RequestMapping("/getHPFeatured")
     public Map<String,Object> getHPFeatured(){
         Map<String,Object> result = new HashedMap();
         List<HPFeatured> hpFeatureds = null;
@@ -35,7 +31,6 @@ public class HPFeaturedController extends BaseDecodedController {
             if (hpFeatureds.size()>0){
                 result.put("data", hpFeatureds);
                 result.put("result",  ResultDict.SUCCESS.getCode());
-
             }else{
                 result.put("result", ResultDict.SYSTEM_ERROR.getCode());
             }
