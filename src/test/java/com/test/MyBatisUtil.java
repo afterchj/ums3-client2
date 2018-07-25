867ff4329e5c25fc7583cba3bc2d04000f56fe39
hongjian.chen <766256898@qq.com>
2018-07-25 14:42:47 +0800

    修改更新下载量代码

--- src/test/java/com/test/MyBatisUtil.java
+++ src/test/java/com/test/MyBatisUtil.java
@@ -1,9 +1,14 @@
 //package com.test;
 //
+//import com.dao.SPFileDao;
+//import com.dao.SPFileDownloadDao;
+//import com.model.SPFile;
 //import org.apache.ibatis.session.SqlSession;
 //import org.apache.ibatis.session.SqlSessionFactory;
 //import org.springframework.context.ApplicationContext;
 //import org.springframework.context.support.ClassPathXmlApplicationContext;
+//
+//import java.util.List;
 //
 //
 //public class MyBatisUtil {
@@ -12,7 +17,7 @@
 //
 //    static {
 //        ApplicationContext act = new ClassPathXmlApplicationContext("classpath:conf/beans.xml");
-//        sessionFactory = (SqlSessionFactory) act.getBean("sqlSessionFactory2");
+//        sessionFactory = (SqlSessionFactory) act.getBean("sqlSessionFactory");
 //    }
 //
 //    public static SqlSession getSess() {
@@ -20,6 +25,9 @@
 //    }
 //
 //    public static void main(String[] args) {
-//
+//        SqlSession session = getSess();
+//        session.getMapper(SPFileDownloadDao.class).updateTotalDownload(1362, "2018-07-18");
+//        List<SPFile> list = session.getMapper(SPFileDao.class).getHottestPage("", "", 0, 24);
+//        System.out.println(list.size());
 //    }
 //}
