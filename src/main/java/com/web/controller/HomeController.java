d3d061c90469d802257e022d90a6178535eece34
hongjian.chen <766256898@qq.com>
2018-07-25 12:54:00 +0800

    优化代码

--- src/main/java/com/web/controller/HomeController.java
+++ src/main/java/com/web/controller/HomeController.java
@@ -23,109 +23,106 @@ import com.web.vo.FrontThemeFileVo;
 
 @Controller("homeController")
 @RequestMapping("/app")
-public class HomeController {
-	
-	private FileStoreInfoService fileStoreInfoService;
-	private AdvertisementService advertisementService;
-	private SPAdvertisementService spadvertisementService;
-	private Logger logger = Logger.getLogger(HomeController.class);
-	
-	
-//	@ExceptionHandler(RuntimeException.class)
-//	public String handleException(RuntimeException re,
-//			HttpServletRequest request) {
-//		logger.error("home.", re);
-//		return null;
-//	}
-	
-	@RequestMapping("/home")
-	public String oldHome(String g, ModelMap model){
-		List<FrontThemeFileVo> fileStoreInfoVos = fileStoreInfoService.getHomePage(g);
+public class HomeController extends BaseDecodedController {
+
+    private FileStoreInfoService fileStoreInfoService;
+    private AdvertisementService advertisementService;
+    private SPAdvertisementService spadvertisementService;
+    private Logger logger = Logger.getLogger(HomeController.class);
+
+
+    @ExceptionHandler(RuntimeException.class)
+    public String handleException(RuntimeException re, HttpServletRequest request) {
+        if (request.getContextPath().indexOf("\\/m\\/") > 0) {
+            return "forword:/app/m/home";
+        } else {
+            return "forward:/app/f/home";
+        }
+    }
+
+    @RequestMapping("/home")
+    public String oldHome(String g, ModelMap model) {
+        List<FrontThemeFileVo> fileStoreInfoVos = fileStoreInfoService.getHomePage(g);
         String top = FrontDisplayUtils.json(advertisementService.getByType("store"));
         String bottom = FrontDisplayUtils.json(advertisementService.getByType("sbot"));
         model.put("tbars", top);
         model.put("bbars", bottom);
         model.put("data", JSONArray.toJSONString(fileStoreInfoVos));
-		return "home";
-	}
-	
-	
-	@RequestMapping("/{gender}/home")
-	public String home(@PathVariable("gender") String gender, ModelMap model){
-		List<FrontThemeFileVo> fileStoreInfoVos = fileStoreInfoService.getHomePage(gender);
-		String top = FrontDisplayUtils.json(advertisementService.getByType("store"));
-        String bottom = FrontDisplayUtils.json(advertisementService.getByType("sbot"));
-        model.put("tbars", top);
-        model.put("bbars", bottom);
-        model.put("data", JSONArray.toJSONString(fileStoreInfoVos));
-		return "home";
-	}
-	
-	@RequestMapping("/home_{num}")
-	public String newHome(@PathVariable("num") Integer num, ModelMap model){
-		try {
-			List<FrontCategoryInfoVo> fileStoreInfoVos = fileStoreInfoService.getNewHomePage(num);
-			String topicBars = FrontDisplayUtils.json(advertisementService.getByType("category"));
-			model.put("tbars", topicBars);
-			model.put("data", JSONArray.toJSONString(fileStoreInfoVos));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return "home";
-	}
+        return "home";
+    }
 
-	@RequestMapping("/sphome")
-	public String newSphome(ModelMap model){
-		try {
+
+    @RequestMapping("/{gender}/home")
+    public String home(@PathVariable("gender") String gender, ModelMap model) {
+        oldHome(gender, model);
+        return "home";
+    }
+
+    @RequestMapping("/home_{num}")
+    public String newHome(@PathVariable("num") Integer num, ModelMap model) {
+        try {
+            List<FrontCategoryInfoVo> fileStoreInfoVos = fileStoreInfoService.getNewHomePage(num);
+            String topicBars = FrontDisplayUtils.json(advertisementService.getByType("category"));
+            model.put("tbars", topicBars);
+            model.put("data", JSONArray.toJSONString(fileStoreInfoVos));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "home";
+    }
+
+    @RequestMapping("/sphome")
+    public String newSphome(ModelMap model) {
+        try {
 //			List<FrontCategoryInfoVo> fileStoreInfoVos = fileStoreInfoService.getNewHomePage(num);
-			String topicBars = FrontDisplayUtils.jsonSp(spadvertisementService.getSpByType("top"));
-			model.put("tbars", topicBars);
+            String topicBars = FrontDisplayUtils.jsonSp(spadvertisementService.getSpByType("top"));
+            model.put("tbars", topicBars);
 //			model.put("data", JSONArray.toJSONString(fileStoreInfoVos));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return "home";
-	}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "home";
+    }
 
-	@RequestMapping("/sprecommend")
-	public String sprecommend(ModelMap model){
-		try {
-			String data = FrontDisplayUtils.jsonSpOfRecommend(spadvertisementService.getSpByType("recommend"));
-			model.put("result", ResultDict.SUCCESS.getCode());
-			model.put("data", data);
-		} catch (Exception e) {
-			model.put("result", ResultDict.SYSTEM_ERROR.getCode());
-			e.printStackTrace();
-		}
-		return "home";
-	}
+    @RequestMapping("/sprecommend")
+    public String sprecommend(ModelMap model) {
+        try {
+            String data = FrontDisplayUtils.jsonSpOfRecommend(spadvertisementService.getSpByType("recommend"));
+            model.put("result", ResultDict.SUCCESS.getCode());
+            model.put("data", data);
+        } catch (Exception e) {
+            model.put("result", ResultDict.SYSTEM_ERROR.getCode());
+            e.printStackTrace();
+        }
+        return "home";
+    }
 
-	@RequestMapping("/sptopic")
-	public String sptopic(ModelMap model){
-		try {
-			String data = FrontDisplayUtils.jsonSpOfTopic(spadvertisementService.getSpByType("topic"));
-			model.put("result", ResultDict.SUCCESS.getCode());
-			model.put("data", data);
-		} catch (Exception e) {
-			model.put("result", ResultDict.SYSTEM_ERROR.getCode());
-			e.printStackTrace();
-		}
-		return "home";
-	}
+    @RequestMapping("/sptopic")
+    public String sptopic(ModelMap model) {
+        try {
+            String data = FrontDisplayUtils.jsonSpOfTopic(spadvertisementService.getSpByType("topic"));
+            model.put("result", ResultDict.SUCCESS.getCode());
+            model.put("data", data);
+        } catch (Exception e) {
+            model.put("result", ResultDict.SYSTEM_ERROR.getCode());
+            e.printStackTrace();
+        }
+        return "home";
+    }
 
 
-	@Autowired
-	public void setFileStoreInfoService(FileStoreInfoService fileStoreInfoService) {
-		this.fileStoreInfoService = fileStoreInfoService;
-	}
+    @Autowired
+    public void setFileStoreInfoService(FileStoreInfoService fileStoreInfoService) {
+        this.fileStoreInfoService = fileStoreInfoService;
+    }
 
-	@Autowired
-	public void setAdvertisementService(AdvertisementService advertisementService) {
-		this.advertisementService = advertisementService;
-	}
+    @Autowired
+    public void setAdvertisementService(AdvertisementService advertisementService) {
+        this.advertisementService = advertisementService;
+    }
 
-	@Autowired
-	public void setSPAdvertisementService(SPAdvertisementService spadvertisementService) {
-		this.spadvertisementService = spadvertisementService;
-	}
+    @Autowired
+    public void setSPAdvertisementService(SPAdvertisementService spadvertisementService) {
+        this.spadvertisementService = spadvertisementService;
+    }
 }
--- src/test/java/com/test/MyBatisUtil.java
+++ src/test/java/com/test/MyBatisUtil.java
@@ -1,35 +1,25 @@
-package com.test;
-
-import com.model.ThirdLogin;
-import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-
-
-public class MyBatisUtil {
-    private static SqlSessionFactory sessionFactory;
-
-
-    static {
-        ApplicationContext act = new ClassPathXmlApplicationContext("classpath:conf/beans.xml");
-        sessionFactory = (SqlSessionFactory) act.getBean("sqlSessionFactory2");
-    }
-    public static SqlSession getSess() {
-        return sessionFactory.openSession();
-    }
-
-    public static void main(String[] args) {
-  SqlSession session = getSess();
-        ThirdLogin thirdLogin = new ThirdLogin();
-        thirdLogin.setId("44");
-
-        //thirdLogin.setQq_image_url("33");
- thirdLogin.setWx_nickname("44");
-        thirdLogin.setWx("44");
-        session.getMapper(ThirdLoginDao.class).updateUserWx(thirdLogin);
-
-
-    }
-}
+//package com.test;
+//
+//import org.apache.ibatis.session.SqlSession;
+//import org.apache.ibatis.session.SqlSessionFactory;
+//import org.springframework.context.ApplicationContext;
+//import org.springframework.context.support.ClassPathXmlApplicationContext;
+//
+//
+//public class MyBatisUtil {
+//    private static SqlSessionFactory sessionFactory;
+//
+//
+//    static {
+//        ApplicationContext act = new ClassPathXmlApplicationContext("classpath:conf/beans.xml");
+//        sessionFactory = (SqlSessionFactory) act.getBean("sqlSessionFactory2");
+//    }
+//
+//    public static SqlSession getSess() {
+//        return sessionFactory.openSession();
+//    }
+//
+//    public static void main(String[] args) {
+//
+//    }
+//}
